343a57e7dfe8b69aefb20d1ff467b268c4c39046
matt <mattevo10@gmail.com>
2022-02-23 15:40:56 +0100

    added search

--- src/components/card-list/card-list.components.jsx
+++ src/components/card-list/card-list.components.jsx
@@ -5,9 +5,10 @@ import { Card } from "../card/card.components";
 import "./card-list.styles.css";
 
 export const CardList = (props) => {
+  const { monsters } = props;
   return (
     <div className="card-list">
-      {props.monsters.map((monster) => (
+      {monsters.map((monster) => (
         <Card key={monster.id} monster={monster} />
       ))}
     </div>
--- src/components/card/card.components.jsx
+++ src/components/card/card.components.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./card.styles.css";
 
 export const Card = (props) => {
+  const { monster } = props;
   return (
     <div className="card-container">
-      <img
-        src={`https://robohash.org/${props.monster.id}?set=set4`}
-        alt="monster"
-      />
-      <h1 key={props.monster.id}>{props.monster.name}</h1>
-      <h2>{props.monster.email}</h2>
+      <img src={`https://robohash.org/${monster.id}?set=set4`} alt="monster" />
+      <h1 key={monster.id}>{monster.name}</h1>
+      <h2>{monster.email}</h2>
     </div>
   );
 };
